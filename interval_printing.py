81d0f1462a946b7fa35407eff0c3123c945cb17f
Donjourr <dongjunane@gmail.com>
2023-02-26 00:15:41 -0500

    number interval printer

---
+++ interval_printing.py
@@ -0,0 +1,10 @@
+#There is a number printer from range -100 to 100.
+#Input an interval of printing numbers
+#Create a tuple that contains the numbers, and print
+
+interval = input('Input the size of interval:')
+numbers = []
+for i in range(-100, 101, int(interval)):
+    numbers.append(i)
+
+print(numbers)
